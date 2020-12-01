acad784d2de0d583141bba2114de9cbe596b4f47
Yao_Z <37908699+00make@users.noreply.github.com>
2020-12-02 01:32:01 +0800

    update

--- Lecture5.R
+++ Lecture5.R
@@ -1,55 +1,54 @@
-# R for MATLAB users:
+# R for MATLAblob users:
 # http://mathesaurus.sourceforge.net/octave-r.html
 set.seed(123)
-A <- matrix(sample(100, 15), nrow = 5, ncol = 3)
+a <- matrix(sample(100, 15), nrow = 5, ncol = 3)
 set.seed(234)
-B <- matrix(sample(100, 15), nrow = 5, ncol = 3)
+blob <- matrix(sample(100, 15), nrow = 5, ncol = 3)
 set.seed(321)
-X <- matrix(sample(100, 25), nrow = 5, ncol = 5)
+x <- matrix(sample(100, 25), nrow = 5, ncol = 5)
 set.seed(213)
 b <- matrix(sample(100, 5), nrow = 5, ncol = 1)
-A
-B
-X
+a
+blob
+x
 b
 #  + - * / ^
 # Element-wise addition, subtraction, multiplication, division
 # , and exponentiation, respectively.
-A + 2
-A * 2
-A^2
+a + 2
+a * 2
+a^2
 
 # Matrix multiplication
-t(A) %*% B
+t(a) %*% blob
 
-# Returns a vector containing the column means of A.
-colMeans(A)
+# Returns a vector containing the column means of a.
+colMeans(a)
 
-# Returns a vector containing the column sums of A.
-colSums(A)
+# Returns a vector containing the column sums of a.
+colSums(a)
 
-# Returns a vector containing the row means of A.
-rowMeans(A)
+# Returns a vector containing the row means of a.
+rowMeans(a)
 
-# Returns a vector containing the row sums of A.
-rowSums(A)
+# Returns a vector containing the row sums of a.
+rowSums(a)
 
 
 # Matrix Crossproduct
-# A'A
-crossprod(A)
-# A'B
-crossprod(A, B)
+# a'a
+crossprod(a)
+# a'blob
+crossprod(a, blob)
 
-# Inverse of A where A is a square matrix.
-solve(X)
+# Inverse of a where a is a square matrix.
+solve(x)
 
 # Solves for vector x in the equation b = Ax.
-# b = Xv
-v <- solve(X, b)
+v <- solve(x, b)
 v
 # Returns a vector containing the elements of the principal diagonal
-diag(X)
+diag(x)
 
 # Creates a diagonal matrix with the elements of x in the principal diagonal.
 diag(c(1, 2, 3, 4))
@@ -57,5 +56,5 @@ diag(c(1, 2, 3, 4))
 # If k is a scalar, this creates a k x k identity matrix.
 diag(5)
 
-# Eigenvalues and eigenvectors of A.
-eigen(X)
+# Eigenvalues and eigenvectors of a.
+eigen(x)
