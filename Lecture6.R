5ae293f437ad63c3dda5e5ebbb5cd45b09e7d5e8
Yao_Z <37908699+00make@users.noreply.github.com>
2020-12-02 01:29:53 +0800

    update

--- Lecture6.R
+++ Lecture6.R
@@ -1,11 +1,11 @@
 # FACTOR
-factor = factor(rep(c(1:3),times=5))
+factor <- factor(rep(c(1:3), times = 5))
 factor
-X = sample(100,15)
+x <- sample(100, 15)
 ?tapply
-tapply(X,factor,mean)
-rbind(X,factor)
-boo = rbind(X,factor)[2,]==1
+tapply(x, factor, mean)
+rbind(x, factor)
+boo <- rbind(x, factor)[2, ] == 1
 which(boo)
-rbind(X,factor)[1,which(boo)]
-sum(rbind(X,factor)[1,which(boo)])/length(which(boo))
+rbind(x, factor)[1, which(boo)]
+sum(rbind(x, factor)[1, which(boo)]) / length(which(boo))
